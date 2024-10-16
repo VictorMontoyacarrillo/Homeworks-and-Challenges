9e7fa7e622e6eb24cb0ce1cf23392ebce42bc4df
Victor Montoya <vamontoya@inorca.com.co>
2024-10-16 10:29:37 -0500

    crear ejercicio 3

---
+++ firsapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FirstApp from './FirstApp';
+
+const App = () => {
+  return (
+    <div>
+      <FirstApp />  {/* Implementamos el componente */}
+    </div>
+  );
+};
+
+export default App;
+
